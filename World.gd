4131e4b7c3afa3576a1499925f4d93f186ea8867
Braidon Lee <braidonl@hotmail.com>
2021-09-18 14:02:31 -0300

    end

--- World.gd
+++ World.gd
@@ -3,12 +3,16 @@ extends Node2D
 onready var hud = $HUD
 onready var obstacle_spawner = $ObstacleSpawner
 onready var ground = $Ground
+onready var menu = $Menu
+
+const SAVE_FILE_PATH = "user://savedata.save"
 
 var score = 0 setget set_score
+var highscore = 0
 
 func _ready():
 	obstacle_spawner.connect("obstacle_created", self, "_on_obstacle_created")
-#	new_game()
+	load_highscore()
 
 func new_game():
 	self.score = 0
@@ -35,3 +39,22 @@ func game_over():
 	obstacle_spawner.stop()
 	ground.get_node("AnimationPlayer").stop()
 	get_tree().call_group("obstacles", "set_physics_process", false)
+	
+	if score > highscore:
+		highscore = score
+		save_highscore()
+	
+	menu.init_game_over_menu(score, highscore)
+
+func save_highscore():
+	var save_date = File.new()
+	save_date.open(SAVE_FILE_PATH, File.WRITE)
+	save_date.store_var(highscore)
+	save_date.close()
+	
+func load_highscore():
+	var save_data = File.new()
+	if save_data.file_exists(SAVE_FILE_PATH):
+		save_data.open(SAVE_FILE_PATH, File.READ)
+		highscore = save_data.get_var()
+		save_data.close()
--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -3,11 +3,13 @@ class_name Player
 
 signal died
 
-export var FLAP_FORCE = -200
+export var FLAP_FORCE = -340
 
 const MAX_ROTATION_DEGREES = -30.0
 
 onready var animator = $AnimationPlayer
+onready var hit = $Hit
+onready var wing = $Wing
 
 var started = false
 var alive = true
@@ -24,22 +26,24 @@ func _physics_process(_delta):
 		
 	if linear_velocity.y > 0:
 		if rotation_degrees <= 90:
-			angular_velocity = 3
+			angular_velocity = 5
 		else:
 			angular_velocity = 0
 	
 func start():
 	if started: return
 	started = true
-	gravity_scale = 5.0
+	gravity_scale = 10.0
 	animator.play("flap")
 
 func flap():
 	linear_velocity.y = FLAP_FORCE
 	angular_velocity = -8.0
+	wing.play()
 
 func die():
 	if !alive: return
 	alive = false
 	animator.stop()
+	hit.play()
 	emit_signal("died")
